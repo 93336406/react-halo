82f1b90089004b18cf936e27504c0cfc679e3dde
eason <cheng.yi@capitalcrux.com>
2018-12-20 09:51:35 +0800

    PageLoading

--- src/components/Loading/PageLoading.js
+++ src/components/Loading/PageLoading.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import {Spin} from 'antd';
+import './PageLoading.less'
 
-export default () => (
-    <div style={{paddingTop: 100, textAlign: 'center'}}>
-        <Spin size="large"/>
-    </div>
-);
+export default ({loading}) => {
+    if (loading) {
+        return <div className={`loading-spinner`} style={{ textAlign: 'center'}}>
+            <Spin size="large"/>
+        </div>;
+    } else {
+        return null;
+    }
+};
